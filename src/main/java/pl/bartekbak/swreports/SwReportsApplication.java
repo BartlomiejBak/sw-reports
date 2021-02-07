e24ef0390a18d9c43d3b2a512d392a7e3590b6d5
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-07 12:50:55 +0100

    feat: initial commit

---
+++ src/main/java/pl/bartekbak/swreports/SwReportsApplication.java
@@ -0,0 +1,13 @@
+package pl.bartekbak.swreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwReportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwReportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bartekbak/swreports/SwReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartekbak.swreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwReportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
