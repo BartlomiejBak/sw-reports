bbc53c682ef8e45d41c5c435f19a951fc4d9206d
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-08 19:53:12 +0100

    feat: add InvalidQueryException, closes #17

---
+++ src/main/java/pl/bartekbak/swreports/exception/InvalidQueryException.java
@@ -0,0 +1,8 @@
+package pl.bartekbak.swreports.exception;
+
+public class InvalidQueryException extends RuntimeException {
+
+    public InvalidQueryException(String message) {
+        super(message);
+    }
+}
