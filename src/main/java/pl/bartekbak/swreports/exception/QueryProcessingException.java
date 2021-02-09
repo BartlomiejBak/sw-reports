5dc4500de35c6a71427471f162ed016f3eb6e6c1
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-09 15:04:59 +0100

    feat: add QueryProcessingException

---
+++ src/main/java/pl/bartekbak/swreports/exception/QueryProcessingException.java
@@ -0,0 +1,8 @@
+package pl.bartekbak.swreports.exception;
+
+public class QueryProcessingException extends RuntimeException {
+
+    public QueryProcessingException(String message) {
+        super(message);
+    }
+}
