549843d5a7465a1d75470401115b603d64b08329
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-10 08:54:45 +0100

    feat: configure Swagger, closes #14

--- src/main/java/pl/bartekbak/swreports/controller/Rest.java
+++ src/main/java/pl/bartekbak/swreports/controller/Rest.java
@@ -1,6 +1,9 @@
 package pl.bartekbak.swreports.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +21,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/reports")
+@Api(value = "/reports")
+@ApiResponses(value = {
+        @ApiResponse(code = 204, message = "No content"),
+        @ApiResponse(code = 400, message = "Bad request"),
+        @ApiResponse(code = 404, message = "Not found")
+})
 public class Rest {
 
     private final ReportService reportService;
