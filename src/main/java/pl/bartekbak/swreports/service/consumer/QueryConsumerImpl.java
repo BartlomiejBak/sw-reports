859bd6f0af9c4ae2f5f30009e2f32e36d0488147
BartlomiejBak <bakbartlomiej88@gmail.com>
2021-02-11 12:07:32 +0100

    feat: rebuild QueryConsumerImpl, improve performance

--- src/main/java/pl/bartekbak/swreports/service/consumer/QueryConsumerImpl.java
+++ src/main/java/pl/bartekbak/swreports/service/consumer/QueryConsumerImpl.java
@@ -2,12 +2,12 @@ package pl.bartekbak.swreports.service.consumer;
 
 import org.springframework.stereotype.Service;
 import pl.bartekbak.swreports.consumer.SWApiConsumer;
-import pl.bartekbak.swreports.converter.PersonToResultConverter;
+import pl.bartekbak.swreports.service.converter.PersonToResultConverter;
 import pl.bartekbak.swreports.dto.Person;
+import pl.bartekbak.swreports.dto.Planet;
 import pl.bartekbak.swreports.dto.Query;
 import pl.bartekbak.swreports.dto.Report;
 import pl.bartekbak.swreports.dto.Result;
-import pl.bartekbak.swreports.exception.QueryProcessingException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,40 +16,54 @@ import java.util.stream.Collectors;
 @Service
 public class QueryConsumerImpl implements QueryConsumer {
 
-    PersonToResultConverter converter;
-    SWApiConsumer client;
+    private static final String GET_PERSON_URI = "http://localhost:8080/api/people/?search=";
+    private static final String GET_PLANET_URI = "http://localhost:8080/api/planets/?search=";
 
-    public QueryConsumerImpl(PersonToResultConverter converter, SWApiConsumer client) {
+    PersonToResultConverter converter;
+    SWApiConsumer consumer;
+
+    public QueryConsumerImpl(PersonToResultConverter converter, SWApiConsumer consumer) {
         this.converter = converter;
-        this.client = client;
+        this.consumer = consumer;
     }
 
     @Override
     public Report createReport(Query query) {
         String characterQuery = query.getCharacterQueryCriteria();
+        String planetQuery = query.getPlanetQueryCriteria();
 
         Report report = Report.builder()
                 .characterQueryCriteria(characterQuery)
-                .planetQueryCriteria(query.getPlanetQueryCriteria())
+                .planetQueryCriteria(planetQuery)
                 .build();
-        try {
-            String address = "http://localhost:8080/api/people/?search=";
-            List<Person> personList = client.getPersonList(address + characterQuery);
-            report.setResultList(getResults(query, personList));
-        } catch (Exception e) {
-            throw new QueryProcessingException(e.getMessage());
-        }
+
+        List<Planet> planets = consumer.getPlanets(GET_PLANET_URI + planetQuery);
+
+        if (planets.isEmpty()) return report;
+        List<Person> personList = consumer.getPersons(GET_PERSON_URI + characterQuery);
+        List<Person> filteredPersonList = filterPlanet(personList, planets);
+
+        report.setResultList(getResults(filteredPersonList));
+
         return report;
     }
 
-    private List<Result> getResults(Query query, List<Person> personList) throws Exception {
+    private List<Result> getResults(List<Person> personList) {
         List<Result> resultList = new ArrayList<>();
         for (Person person : personList) {
-            resultList.addAll(converter.convertToResultList(person).stream()
-                    .filter(result -> result.getPlanetName().equals(query.getPlanetQueryCriteria()))
-                    .collect(Collectors.toList()));
+            resultList.addAll(converter.convertToResultList(person));
         }
         return resultList;
+    }
+
+    private List<Person> filterPlanet(List<Person> personList, List<Planet> planets) {
+        List<Person> filteredPersons = new ArrayList<>();
+        for (Planet planet : planets) {
+            filteredPersons.addAll(personList.stream()
+                    .filter(person -> person.getHomeworld().equals(planet.getUrl()))
+                    .collect(Collectors.toList()));
+        }
+        return filteredPersons;
     }
 
 }
